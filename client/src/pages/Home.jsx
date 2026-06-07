e8363414ddaac7d9f6515b9054ac17fce1320e57
Aliza Maryam <your-github-email@example.com>
2026-06-08 01:46:02 +0500

    Aliza Maryam - Added Testimonials component and customer reviews section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Description from "../components/Description"
 import Header from "../components/Header"
 import Steps from "../components/steps"
+import Testimonials from "../components/Testimonials"
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header/>
       <Steps/>
       <Description/>
+      <Testimonials/>
     </div>
   )
 }
